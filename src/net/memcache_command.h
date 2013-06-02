8555b6c81fc50e2124aeaa29ddfeaef484e3fc26
Allen Lee <allenlee.lz@gmail.com>
2013-06-02 12:11:03 +0800

    Support multi-key get reponse
    Conflicts:
    	src/net/capture_engine.cpp
    	src/net/memcache_command.cpp
    	src/net/memcache_command.h

--- src/net/memcache_command.h
+++ src/net/memcache_command.h
@@ -32,11 +32,17 @@ class MemcacheCommand
   // only when isRequest is true
   std::string getCommandName() const { return commandName_; }
 
+  ssize_t getObjectNumber() const { return objectKeyList_.size(); }
+
   // sometimes when isResponse is true, sometimes when isRequest is true
-  std::string getObjectKey() const { return objectKey_; }
+  std::string getObjectKey(uint32_t idx = 0) const { 
+    return (idx < objectKeyList_.size() ? objectKeyList_[idx] : ""); 
+  }
 
   // only when isResponse is true
-  uint32_t getObjectSize() const { return objectSize_; }
+  uint32_t getObjectSize(uint32_t idx = 0) const { 
+      return (idx < objectSizeList_.size() ? objectSizeList_[idx] : 0); 
+  }
 
   // source address for request
   std::string getSourceAddress() const { return sourceAddress_; }
@@ -55,6 +61,11 @@ class MemcacheCommand
   MemcacheCommand(const memcache_command_t cmdType,
                   const std::string sourceAddress,
                   const std::string destinationAddress,
+                  const std::string commandName);
+
+  MemcacheCommand(const memcache_command_t cmdType,
+                  const std::string sourceAddress,
+                  const std::string destinationAddress,
                   const std::string commandName,
                   const std::string objectKey,
                   uint32_t objectSize);
@@ -68,12 +79,14 @@ class MemcacheCommand
                                              std::string sourceAddress,
                                              std::string destinationAddress);
 
+  void pushObject(const std::string objectKey, uint32_t objectSize);
+
   memcache_command_t cmdType_;
   std::string sourceAddress_;
   std::string destinationAddress_;
   std::string commandName_;
-  std::string objectKey_;
-  uint32_t objectSize_;
+  std::vector<std::string> objectKeyList_;
+  std::vector<uint32_t> objectSizeList_;
 };
 
 } // end namespace
